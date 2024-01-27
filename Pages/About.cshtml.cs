e1f0882671fa1b3a0ee42945eaf8612819d1694e
Flávio Eduardo <xflax00@gmail.com>
2024-01-27 15:53:41 -0300

    home and about pages added

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace flavioedu_dev.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Skills.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace flavioedu_dev.Pages
+{
+    public class SkillsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
